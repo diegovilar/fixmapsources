9fa6f22f432fa8aa39da66d0d09b56d30646a0c6
Diego Vilar <diegovilar@gmail.com>
2017-05-31 11:05:22 -0300

    Work in progress

--- src/abstract/program-arguments.ts
+++ src/abstract/program-arguments.ts
@@ -2,8 +2,10 @@ export interface IProgramArguments {
 
     _: string[];
     s: string;
+    rx: string;
     r: string;
     search: string;
+    regex: string;
     replace: string;
     v: boolean;
     verbose: boolean;
--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,49 @@ import * as yargs from "yargs";
 import { Fixer } from "./lib";
 import { IProgramArguments } from "./abstract/program-arguments";
 
+const NON_REGEX_TEMPLATE = "^(?:/|(?:/?\\.{1,2}/){0,})?";
+
 export async function main(args?: string[]) {
 
     const aux = yargs
         .epilog("Copyright 2017 - Diego Vilar (https://github.com/diegovilar)")
-        .usage("Usage: fixmapsources [-v] [-s <search-regex>] -r <replacement> <input-glob>")
+        .usage("Usage: fixmapsources [-v] [-rx] -s <search> -r <replacement> <input-glob>")
+        .alias("rx", "regex")
+            .describe("re", "Treats search term as a Regular Expression")
         .alias("s", "search")
-            .default("s", '^(?:/|(?:/?\\.{1,2}/){0,})?src/')
-            .describe("s", "Regular expression to search for")
+        .describe("s", "Term to search for")
         .alias("r", "replace")
-            .describe("r", "Replacement string")
+        .describe("r", "Replacement string")
         .alias("v", "verbose")
         .alias("h", "help")
-            .help("h")
-        .demandOption(["replace"])
+        .help("h")
+        .demandOption(["search", "replace"])
         .demandCommand(1);
 
     const ARGV: IProgramArguments = args ? aux.parse(args) : aux.argv;
     const FILES_PATTERN = ARGV._[0];
     const REPLACEMENT = ARGV.replace;
     const VERBOSE = ARGV.verbose;
-    let searchPattern = new RegExp(ARGV.search);
+    const REGEX = ARGV.regex;
+    const SEARCH = ARGV.search;
+
+    let pattern: RegExp;
+
+    if (REGEX) {
+        pattern = new RegExp(SEARCH);
+    }
+    else {
+        pattern = new RegExp(NON_REGEX_TEMPLATE + SEARCH);
+    }
 
     const fixer = new Fixer(VERBOSE);
 
     if (VERBOSE) {
-        fixer.log(`Replacing /${searchPattern.source}/ for "${REPLACEMENT}" in ${FILES_PATTERN}`);
+        fixer.log(`Replacing /${pattern.source}/ for "${REPLACEMENT}" in ${FILES_PATTERN}`);
     }
 
     try {
-        const files = await fixer.fixFiles(FILES_PATTERN, searchPattern, ARGV.replace);
+        const files = await fixer.fixFiles(FILES_PATTERN, pattern, ARGV.replace);
 
         if (VERBOSE) {
             fixer.log(`${files.length} file(s) processed`);
